9aba869c9a294cb35b2854a56648f43972c9efa0
robertrottermann <robert@redcor.ch>
2016-07-31 14:10:02 +0200

    added lots of comments

--- config/base_info.py
+++ config/base_info.py
@@ -1,1 +1,1 @@
-base_info = {'docker_path_map': ('/home/robert/', '/root/'), 'project_path': '/home/robert/projects'}
+base_info = {'odoo_server_data_path': '/home/robert/odoo_instances', 'docker_path_map': ('/home/robert/', '/root/'), 'project_path': '/home/robert/projects'}
